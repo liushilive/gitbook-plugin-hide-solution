e5d6570778a94dd5e7442c7942ac8609f8a3142c
Yiu Man Chi <ymcatar@gmail.com>
2015-06-10 09:53:06 +0800

    Prepare to switch to inline html

--- assets/sectionx.js
+++ assets/sectionx.js
@@ -1,62 +1,6 @@
-var clickAction = function clickAction(source, tar){
-
-	source.click(function(){
-
-		target = '#' + tar;
-
-		$(target).on('show.bs.collapse', function(){
-			if(source.attr('hide'))
-				source.html("<b>"+ source.attr('hide') +"</b><span class='fa fa-angle-up pull-left'/>");
-			else
-				source.html("<span class='fa fa-angle-up'/>");
-		});
-
-		$(target).on('hide.bs.collapse', function(){
-			if(source.attr('show'))
-				source.html("<b>"+ source.attr('show') +"</b><span class='fa fa-angle-down pull-left'/>");
-			else
-				source.html("<span class='fa fa-angle-down'/>");
-		});
-
-		$(target).collapse("toggle");
-
-	});
-};
-
 var init = function init(){
 
-	$('.section').each(function(){
-
-		if($(this).attr('show'))
-			$(this).html("<b>"+ $(this).attr('show') +"</b><span class='fa fa-angle-down pull-left'/>");
-		else
-			$(this).html("<span class='fa fa-angle-down'/>");
-
-		if($(this).hasClass('atTitle'))
-			$(this).addClass('btn').addClass('btn-default');
-		else
-			$(this).addClass('btn').addClass('btn-primary');
-
-		clickAction($(this), $(this).attr('target'));
-	});	
-
-	if($('div.book').hasClass('color-theme-2'))
-		$('.panel').each(function(){
-			$(this).addClass('dark');
-		});
-
-	$('#color-theme-preview-0, #color-theme-preview-1').click(function(){
-		$('.panel').each(function(){
-			$(this).removeClass('dark');
-		});	
-	});
-
-	$('#color-theme-preview-2').click(function(){
-		$('.panel').each(function(){
-			if($(this).hasClass('dark') === false)
-				$(this).addClass('dark');
-		});
-	});
+	[insert jQuery here]
 
 };
 
--- index.js
+++ index.js
@@ -14,34 +14,4 @@ module.exports = {
 			"sectionx.css"
 		]
 	},
-
-	blocks: {
-
-		section: {
-			process: function(blk) {
-
-				var data = '';
-
-					data+=	'.panel.panel-default' + '\n';
-					data+=	'	.panel-heading' + '\n';
-
-				if(blk.kwargs.show) {
-					data+=	'		h2 #{title}' + '\n';
-					data+=	'			a.pull-right.section.atTitle(target="' + blk.kwargs.id + '")' + '\n';
-					data+=	'				span.fa.fa-times' + '\n';
-					data+=	'	.panel-collapse.collapse.in(id=sectionName)' + '\n';
-				}
-				else
-				{
-					data+=	'		h2 #{title}' + '\n';
-					data+=	'	.panel-collapse.collapse(id=sectionName)' + '\n';
-				}
-	
-					data+=	'		.panel-body' + '\n';
-					data+=	'			!=content' + '\n';
-
-				return jade.render(data, {title: blk.kwargs.title, content: marked(blk.body), sectionName: blk.kwargs.id});
-			}
-		},
-	}
 };
